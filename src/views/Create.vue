d8119dfa18efdaa46e568f9bd23e43e67e2742fd
Bijaya Giri <bijayagiri@Bijayas-MacBook-Pro.local>
2022-09-06 20:42:52 +0545

    login-updated

---
+++ src/views/Create.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>HELO </h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: "create",
+  setup() {
+    // Create data
+
+    // Add exercise
+
+    // Delete exercise
+
+    // Listens for chaging of workout type input
+
+    // Create workout
+
+    return {};
+  },
+};
+</script>
