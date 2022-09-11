388bfc1b91200ab2cbb88a2ed375fb3d69196b40
Bijaya Giri <jaya.jc999@gmail.com>
2022-09-11 16:42:44 +0545

    my first test (#1)
    Co-authored-by: Bijaya Giri <bijayagiri@Bijayas-MacBook-Pro.local>

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,33 +1,48 @@
 <template>
   <header class="bg-at-light-green text-white">
-    <nav class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row">
-    <div class="flex items-center gap-x-4">
-      <img class="w-14" src="../assets/images/dumbbell-light.png" alt="" />
-      <h1 class="text-log">Active Tracer</h1>
-    </div>
+    <nav
+      class="container py-5 px-4 flex flex-col gap-4 items-center sm:flex-row"
+    >
+      <div class="flex items-center gap-x-4">
+        <img class="w-14" src="../assets/images/dumbbell-light.png" alt="" />
+        <h1 class="text-log">Bijaya Giri Active Tracer</h1>
+      </div>
 
-<ul class="flex flex-1 justify-end gap-x-10">
-  <router-link class="cursor-pointer" :to="{ name:'Home' }">Home </router-link>
-  <!-- <router-link class="cursor-pointer" :to="{ name:'Create' }">Create </router-link> -->
-    <router-link class="cursor-pointer" :to="{ name:'Login' }">Login </router-link>
-  <li class="cursor-pointer">Logout </li>
-</ul>
-
-</nav>
-
+      <ul class="flex flex-1 justify-end gap-x-10">
+        <router-link class="cursor-pointer" :to="{ name: 'Home' }"
+          >Home
+        </router-link>
+        <router-link v-if="user" class="cursor-pointer" :to="{ name: 'Create' }"
+          >Create
+        </router-link>
+        <router-link v-if="!user" class="cursor-pointer" :to="{ name: 'Login' }"
+          >Login
+        </router-link>
+        <li @click="logout" v-if="user" class="cursor-pointer">Logout</li>
+      </ul>
+    </nav>
   </header>
 </template>
 
 <script>
+import { supabase } from "../supabase/init";
+import { useRouter } from "vue-router";
+import { computed } from "@vue/reactivity";
+import store from "../store";
+
 export default {
   setup() {
     // Get user from store
-
+    const user = computed(() => store.state.user);
     // Setup ref to router
- 
+    const router = useRouter();
     // Logout function
+    const logout = async () => {
+      await supabase.auth.signOut();
+      router.push({ name: "Home" });
+    };
 
-    return {};
+    return { logout, user };
   },
 };
 </script>
